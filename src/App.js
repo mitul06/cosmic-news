6fbe541c09e81eeff12584a6c7cb69634ff47798
Mitul Sartanpara <mitulsartanpara0607@gmail.com>
2022-01-09 23:19:38 +0530

    feat: added navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import Navbar from './components/Navbar/Navbar'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
 
-function App() {
+function App () {
+  
+  // useEffect(() => {
+  //   const options = {
+  //     method: 'GET',
+  //     url: 'https://free-news.p.rapidapi.com/v1/search',
+  //     params: {q: 'Elon Musk', lang: 'en'},
+  //     headers: {
+  //       'x-rapidapi-host': 'free-news.p.rapidapi.com',
+  //       'x-rapidapi-key': 'cd61582f8amsh5065293a7ca81bep1c2904jsn8b3f7d99cd2a'
+  //     }
+  //   }
+
+  //   axios.request(options).then((res) => {
+  //     console.log(res, "res");
+  //   }).catch(err => {console.log(err);})
+
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+      <Router>
+      <Navbar />
+        <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about'  element={<About />}  />
+        <Route exact path='/contact'  element={<Contact />}  />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
